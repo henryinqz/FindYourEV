812028109621406b89789050dcacbc9e2d8f3bbf
Henry <36207470+henryinqz@users.noreply.github.com>
2021-01-23 10:07:35 -0500

    get_models_from_years func

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,25 @@
 from typing import TextIO, List, Dict, Union
 
-BRAND = 0
-MODEL = 1
-YEAR = 2
-POWER = 3
-DRIVETRAIN = 4
-FORM_FACTOR = 5
-PRICE = 6
-EV_TYPE = 7
-SAFETY_RATING = 8
-RANGE = 9
-CHARGING = 10
-FEATURES = 11
+MIN_YR = "min_year"
+MAX_YR = "max_year"
+
+CONSTANT = 0
+INDEX = 1
+BRAND = ["brand", 0]
+MODEL = ["model", 1]
+YEAR = ["year", 2]
+POWER = ["power", 3]
+DRIVETRAIN = ["drivetrain", 4]
+FORM_FACTOR = ["form_factor", 5]
+PRICE = ["price", 6]
+EV_TYPE = ["ev_type", 7]
+SAFETY_RATING = ["safety_rating", 8]
+RANGE = ["range", 9]
+CHARGING = ["charging", 10]
+FEATURES = ["features", 11]
 
 def clean_data(input_file: TextIO) -> Dict:
-    factors = input_file.readline().strip().split(",")
+    input_file.readline().strip().split(",") # First row
 
     cars = {}
     for info in database.readlines():
@@ -22,23 +27,26 @@ def clean_data(input_file: TextIO) -> Dict:
         for index in range(len(info_list)):
             info_list[index] = info_list[index].strip()
 
-        cars[info_list[MODEL]] = {
-            "brand": info_list[BRAND],
-            "year": info_list[YEAR],
-            "power": info_list[POWER],
-            "drivetrain": info_list[DRIVETRAIN],
-            "form_factor": info_list[FORM_FACTOR],
-            "price": info_list[PRICE],
-            "ev_type": info_list[EV_TYPE],
-            "safety_rating": info_list[SAFETY_RATING],
-            "range": info_list[RANGE],
-            "charging": (info_list[CHARGING] == True),
-            "features": info_list[FEATURES]
+        cars[info_list[MODEL[INDEX]]] = {
+            BRAND[CONSTANT]: info_list[BRAND[INDEX]],
+            YEAR[CONSTANT]: int(info_list[YEAR[INDEX]]),
+            POWER[CONSTANT]: info_list[POWER[INDEX]],
+            DRIVETRAIN[CONSTANT]: info_list[DRIVETRAIN[INDEX]],
+            FORM_FACTOR[CONSTANT]: info_list[FORM_FACTOR[INDEX]],
+            PRICE[CONSTANT]: info_list[PRICE[INDEX]],
+            EV_TYPE[CONSTANT]: info_list[EV_TYPE[INDEX]],
+            SAFETY_RATING[CONSTANT]: info_list[SAFETY_RATING[INDEX]],
+            RANGE[CONSTANT]: info_list[RANGE[INDEX]],
+            CHARGING[CONSTANT]: (info_list[CHARGING[INDEX]] == True),
+            FEATURES[CONSTANT]: info_list[FEATURES[INDEX]]
         }
     return cars
 
 def search_data(car_data: Dict, search: List[List]) -> List[str]:
-    # specification = [[price, {min, max}], [form_factor, {type, seats}]]
+    # specification = [
+    #     [price, {min, max}],
+    #     [form_factor, {type, seats}]
+    #     ]
     # return list of model names that match search query
     searched_data = []
     for specification, query in search:
@@ -79,9 +87,21 @@ def get_models_from_brands(car_data: Dict, brands: List[str]) -> List[str]:
     
     return models_from_search_brands
 
+def get_models_from_years(car_data: Dict, years: Dict[str, int]) -> List[str]:
+    # years = {MIN_YEAR, MAX_YEAR}
+    models_from_search_years = []
+
+    for model in car_data:
+        model_data = car_data[model]
+        if years[MIN_YR] <= model_data[YEAR[CONSTANT]] <= years[MAX_YR]:
+                models_from_search_years.append(model)
+    
+    return models_from_search_years
+
 
 
 database = open("ev_database.csv")
 data = clean_data(database)
 # print(data)
 # print(get_models_from_brands(data, "Audi"))
+# print(get_models_from_years(data, {MIN_YR: 2020, MAX_YR: 2020}))
